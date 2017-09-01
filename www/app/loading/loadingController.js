61ae5667ab563facc0fda1793bb9d03b6ca6626e
Pedro Costa <costa.pedrodf@gmail.com>
2017-09-01 22:16:47 +0100

    "implemented a retry button in case the health check fails so the user can check again"

--- www/app/loading/loadingController.js
+++ www/app/loading/loadingController.js
@@ -3,18 +3,26 @@
 app.controller('LoadingController', function($scope, $http, $timeout, $state) {
 
     $scope.isLoading = true;
-    $scope.retry = false;
+    $scope.showRetry = false;
 
-    $timeout( () => {
-        $http.get("https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/health").then(function(result) {
-            if(result.status === 200) {
-                console.log("Status", result.status);
-                $state.go("questions");
-            } else {
-                $scope.retry = true;
-            }
-            $scope.isLoading = false;
-        });
-    }, 2000);
+    let getQuestions = () => {
+        $timeout(() => {
+            $http.get("https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/health").then(function(result) {
+                if(result.status === 200) {
+                    console.log("Status", result.status);
+                    $state.go("questions");
+                } else {
+                    $scope.showRetry = true;
+                }
+                $scope.isLoading = false;
+            });
+        }, 3000);
+    }
+    getQuestions();
+
+    $scope.retryAction = () => {
+        $scope.showRetry = false;
+        getQuestions();
+    }
 
 });
