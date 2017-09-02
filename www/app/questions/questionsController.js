a144690128f39d24868cab767121ab864f3bee40
Pedro Costa <costa.pedrodf@gmail.com>
2017-09-02 02:21:06 +0100

    ! loading 10 records at a time; loading more 10 if demanded by user;

--- www/app/questions/questionsController.js
+++ www/app/questions/questionsController.js
@@ -1,9 +1,17 @@
 'use strict';
 
 app.controller('QuestionsController', function($scope, $http, $state) {
-    $http.get('https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/questions?limit&offset&filter').then(function(result) {
-        console.log("Questions", result.data);
+
+    $scope.emptyList = true; // to show 'load more' button after question's list is loaded
+
+    let limit = 10; // the app should fetch 10 records at a time
+    let offset = 10; // the app should fetch 10 records at a time
+    let filter = '';
+
+    $http.get('https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/questions?' + limit + '&' + offset + '&filter').then(function(result) {
+        console.log('Questions', result.data);
         $scope.questions = result.data;
+        $scope.emptyList = false;
     });
 
     $scope.viewQuestion = question => {
@@ -14,6 +22,16 @@ app.controller('QuestionsController', function($scope, $http, $state) {
         .then(result => {
             let questionDetails = result.data;
             $state.go('details', {clickedQuestion:clickedQuestion, question:questionDetails});
+        });
+    }
+
+    // the app should start loading 10 additional records if the user shows intent to browse additional records
+    $scope.loadMoreRecords = () => {
+        limit += 10;
+        offset += 10;
+        $http.get('https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/questions?' + limit + '&' + offset + '&filter').then(function(result) {
+            console.log('Loaded questions', result.data);
+            $scope.questions = result.data;
         });
     }
 
