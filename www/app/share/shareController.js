92b3497d55886bf6d9656c15c6a02e55ed5edddd
Pedro Costa <costa.pedrodf@gmail.com>
2017-09-02 04:19:48 +0100

    ! missing share screen files from last commit;

---
+++ www/app/share/shareController.js
@@ -0,0 +1,40 @@
+'use strict';
+
+app.controller('ShareController', function($scope, $stateParams, $http, $timeout) {
+
+    $scope.urlShared = false;
+    $scope.notSending = true;
+    $scope.showNOKMessage = false;
+    $scope.showOtherNOKMessage = false;
+
+    $scope.destinationEmail = undefined;
+    $scope.clickedQuestion = $stateParams.clickedQuestion;
+    $scope.question = $stateParams.question;
+    $scope.contentUrl = 'https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/questions/' + $scope.clickedQuestion;
+
+    let shareToEmail = () => {
+        $timeout(() => {
+            $http.post('https://private-anon-fc1b5cfd0c-blissrecruitmentapi.apiary-mock.com/share?' + $scope.destinationEmail + '&' + $scope.contentUrl)
+            .then(function(result) {
+                if(result.status === 200) {
+                    console.log("Status", result.status);
+                    $scope.urlShared = true;
+                } else if(result.status === 400) {
+                    $scope.statusText = result.statusText;
+                    $scope.showNOKMessage = true;
+                } else {
+                    $scope.showOtherNOKMessage = true;
+                }
+                $scope.notSending = true;
+            });
+        }, 3000);
+    }
+
+    $scope.share = () => {
+        $scope.urlShared = false;
+        $scope.notSending = false;
+        $scope.showNOKMessage = false;
+        $scope.showOtherNOKMessage = false;
+        shareToEmail();
+    }
+});
